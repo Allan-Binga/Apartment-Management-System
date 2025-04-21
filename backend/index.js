b9ca8ea82cce2f4deca7ce809d8b72a1ad77b0a4
Allan-Binga <allanbinga73@gmail.com>
2025-04-20 21:50:40 -0400

    Adding cors to the main server file.

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const cookieParser = require("cookie-parser");
+const cors = require("cors")
 const authRoute = require("./routes/auth");
 const usersRoute = require("./routes/users");
-const listingsRoute = require("./routes/listings")
-const paymentRoute = require("./routes/payment")
+const listingsRoute = require("./routes/listings");
+const paymentRoute = require("./routes/payment");
 
-require("./config/db")
+require("./config/db");
 
 dotenv.config();
 const app = express();
@@ -14,13 +15,16 @@ const app = express();
 app.use(express.json());
 
 //Cookie Parser
-app.use(cookieParser())
+app.use(cookieParser());
+
+//CORS
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
 // Routes
 app.use("/murandi/v1/auth", authRoute);
 app.use("/murandi/v1/users", usersRoute);
-app.use("/murandi/v1/listings", listingsRoute)
-app.use("/murandi/v1/payments", paymentRoute)
+app.use("/murandi/v1/listings", listingsRoute);
+app.use("/murandi/v1/payments", paymentRoute);
 
 app.listen(5700, () => {
   console.log("Server started on port 5700");
---
+++ client/src/apiEndpoint.js
@@ -0,0 +1,2 @@
+//Backend Endpoint
+export const endpoint = process.env.BACKEND_ENDPOINT
