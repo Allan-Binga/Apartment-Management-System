37b8358f6e6a436d2fbc68a40d5da39d5ff248ec
Allan-Binga <allanbinga73@gmail.com>
2025-04-22 05:19:37 -0400

    Stripe backend implementation completed.

--- backend/controllers/rentCheckout.js
+++ backend/controllers/rentCheckout.js
@@ -7,21 +7,42 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 const createRentCheckoutSession = async (req, res) => {
   const tenantId = req.tenantId;
-  const { amountPaid, paymentDate } = req.body;
+
   try {
-    //Save as 'pending' first
+    //Find Tenant Apartment's Number
+    const tenantQuery = `SELECT apartmentnumber FROM tenants WHERE id = $1`;
+    const tenantResult = await client.query(tenantQuery, [tenantId]);
+
+    if (tenantResult.rows.length === 0) {
+      return res.status(404).json({ error: "Tenant not found" });
+    }
+
+    const apartmentNumber = tenantResult.rows[0].apartmentnumber;
+
+    //Get rent amount from apartment_listings
+    const rentQuery = `SELECT price FROM apartment_listings WHERE apartmentnumber = $1`;
+    const rentResult = await client.query(rentQuery, [apartmentNumber]);
+
+    if (rentResult.rows.length === 0) {
+      return res.status(404).json({ error: "Apartment listing not found" });
+    }
+
+    const rentAmount = rentResult.rows[0].price;
+
+    //Current Date
+    const today = new Date().toISOString().split("T")[0];
+
     const insertPaymentQuery = `
       INSERT INTO payment (tenantid, amountpaid, paymentdate, paymentmethod, paymentstatus)
       VALUES ($1, $2, $3, $4, $5)
       RETURNING paymentid
     `;
 
-    const values = [tenantId, amountPaid, paymentDate, "stripe", "pending"];
-
+    const values = [tenantId, rentAmount, today, "stripe", "pending"];
     const result = await client.query(insertPaymentQuery, values);
     const paymentId = result.rows[0].paymentid;
 
-    //Stripe checkout session
+    //Stripe Session
     const session = await stripe.checkout.sessions.create({
       payment_method_types: ["card"],
       line_items: [
@@ -31,7 +52,7 @@ const createRentCheckoutSession = async (req, res) => {
             product_data: {
               name: `Rent Payment for Tenant ${tenantId}`,
             },
-            unit_amount: amountPaid * 100,
+            unit_amount: rentAmount * 100, // Cents
           },
           quantity: 1,
         },
@@ -43,12 +64,17 @@ const createRentCheckoutSession = async (req, res) => {
         paymentId: paymentId.toString(),
         tenantId: tenantId.toString(),
       },
+      payment_intent_data: {
+        metadata: {
+          paymentId: paymentId.toString(),
+          tenantId: tenantId.toString(),
+        },
+      },
     });
 
     res.status(200).json({ id: session.id, url: session.url });
   } catch (error) {
     console.error("Error creating rent checkout session:", error.message);
-    console.log(error)
     res.status(500).json({ error: "Failed to create checkout session." });
   }
 };
